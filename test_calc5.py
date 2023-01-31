55d82dc8f032055f8e32a422b89e6748590805ee
ricky-kiva <ricky.kiva@gmail.com>
2023-01-31 20:10:13 +0700

    good test structure

--- test_calc5.py
+++ test_calc5.py
@@ -6,6 +6,10 @@ from calc5 import square
 def test_square():
     assert square(2) == 4
     assert square(3) == 9
-    assert square(-2) == 4
-    assert square(-3) == 9
+
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
+
+def test_zero():
     assert square(0) == 0
