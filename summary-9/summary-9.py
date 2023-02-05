3c21c4ef97bdddf47ca784f1fe1f7cdf935ef0ce
ricky-kiva <ricky.kiva@gmail.com>
2023-02-05 13:44:43 +0700

    set

---
+++ summary-9/summary-9.py
@@ -0,0 +1,14 @@
+students = [
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+    {"name": "Padma", "house": "Ravenclaw"}
+]
+
+houses = set() # a 'set' is an array to store ONLY unique value
+for student in students:
+    houses.add(student["house"])
+
+print(sorted(houses))
+print()
